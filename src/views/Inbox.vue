28d90140218def2f3b15fd9e1e36f0fb25a94a2f
Ithil <ithilium@gmail.com>
2020-10-23 22:51:32 +0200

    Add rudimentary inbox

--- src/views/Inbox.vue
+++ src/views/Inbox.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="inbox">
-    <h1>This will become the Inbox</h1>
+    <h1>Inbox</h1>
+    <ul id="fleetingNotes">
+      <li v-for="f in fleetingNotes" :key="f.name">
+        <h2>{{ f.name }}</h2>
+        <div v-html="md.render(f.content)"></div>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import fs from 'fs'
+import path from 'path'
+import MarkdownIt from 'markdown-it'
+
+
+export default {
+  name: 'Inbox',
+  components: {
+  },
+  data() {
+    return {
+      inbox: new this.$global.pensieve.Inbox(this.$global.currentNoteCollection),
+      fleetingNotes: null,
+      md: new MarkdownIt(),
+    }
+  },
+  mounted() {
+    var listing = fs.readdirSync(this.inbox.path)
+    listing = listing.filter(f => /\.(md|txt)$/.test(f))
+    var fleetingNotes = []
+    for (var f of listing) {
+      var content = fs.readFileSync(path.join(this.inbox.path, f), 'utf-8')
+      fleetingNotes.push({
+        name: f,
+        content: content,
+      })
+    }
+    this.fleetingNotes = fleetingNotes
+    console.log(fleetingNotes)
+  }
+}
+</script>

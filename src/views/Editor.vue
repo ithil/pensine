4e8ac5cad5e73ed4f0d1b69e8605204d2c09ceee
Ithil <ithilium@gmail.com>
2020-10-24 21:48:44 +0200

    Display note label and id in editor view

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="editorView">
-    <Vimish></Vimish>
+  <div>
+    <div id="noteMetadata" v-if="currentNote">
+      <span class="noteId">{{ currentNote.id }}</span>
+      <span class="noteLabel">{{ currentNote.label }}</span>
+    </div>
+    <div class="editorView">
+      <Vimish></Vimish>
+    </div>
   </div>
 </template>
 
@@ -12,12 +18,34 @@ export default {
   name: 'Editor',
   components: {
     Vimish
-  }
+  },
+  computed: {
+    currentNote() {
+      return this.$store.state.currentNote
+    }
+  },
 }
 </script>
 
 <style lang="scss">
 .editorView {
   margin-top: 20px;
+}
+#noteMetadata {
+  padding: 10px;
+  font-family: 'Lucida Grande';
+  background-color: #dddddd;
+}
+.noteId {
+  padding: 3px;
+  margin: 2px;
+  background-color: #afdfff;
+  border-radius: 5px;
+  border: 1px solid #7eccff;
+  font-size: 14px;
+}
+.noteLabel {
+  font-weight: bold;
+  margin-left: 5px;
 }
 </style>

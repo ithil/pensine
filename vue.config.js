62f16c9b7f06d666dd0c1cf760ec1af39225a5c4
Ithil <ithilium@gmail.com>
2021-03-02 10:52:20 +0100

    Node Externals fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
+const nodeExternals = require("webpack-node-externals")
+
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      // List native deps here if they don't work
+      externals: ['pensieve'],
+      // If you are using Yarn Workspaces, you may have multiple node_modules folders
+      // List them all here so that VCP Electron Builder can find them
+      nodeModulesPath: ['../../node_modules', './node_modules']
     }
   },
   runtimeCompiler: true

2bd9ce4c43561c623efa465513e64a4ccd6adfcd
Ithil <ithilium@gmail.com>
2021-03-19 22:05:10 +0100

    Recently changed notes on Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,16 @@
       <li>
         Number of notes: {{ currentNoteCollection.allNotes.length }}
       </li>
+      <li>
+        Recently changed notes:
+        <ul>
+          <li v-for="n in recentlyChangedNotes" :key="n.id">
+            <span class="noteLink" @click="$router.push(`/editor/${n.id}`)">
+              {{n.name}}
+            </span>
+          </li>
+        </ul>
+      </li>
     </ul>
   </div>
 </template>
@@ -19,7 +29,12 @@ export default {
   computed: {
     currentNoteCollection() {
       return this.$store.state.currentNoteCollection
-    }
+    },
+    recentlyChangedNotes() {
+      return [...this.currentNoteCollection.allNotes]
+            .sort((a, b) => b.lastModifiedContent - a.lastModifiedContent)
+            .slice(0, 10)
+    },
   },
 }
 </script>
@@ -28,5 +43,8 @@ export default {
 .home {
   padding: 10px;
   font-family: 'PT Mono';
+}
+.noteLink {
+  cursor: pointer;
 }
 </style>

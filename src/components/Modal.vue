42b0a8b9c28e29b34ecf5495f5c8f678659fb7c6
Ithil <5417774+ithil@users.noreply.github.com>
2021-12-11 20:23:00 +0100

    Update modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,20 +1,22 @@
 <template id='#item-template'>
-  <div class="modal" v-if="show" @keydown="keymonitor">
-    <div class="modal__backdrop" @click="closeModal()"/>
+  <div class="modal">
+    <div class="modal__backdrop" @click="closeModal()" v-if="show"/>
 
-    <div class="modal__dialog" ref="dialog" tabindex="23">
-      <div class="modal__header">
-        <slot name="header"/>
-      </div>
+    <transition name="slide">
+      <div class="modal__dialog" ref="dialog" tabindex="23" v-if="show" @keydown="keymonitor">
+        <div class="modal__header">
+          <slot name="header"/>
+        </div>
 
-      <div class="modal__body">
-        <slot name="body"/>
-      </div>
+        <div class="modal__body">
+          <slot name="body"/>
+        </div>
 
-      <div class="modal__footer">
-        <slot name="footer"/>
+        <div class="modal__footer">
+          <slot name="footer"/>
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -59,49 +61,60 @@
 </script>
 
 <style lang="scss">
-.modal {
-  overflow-x: hidden;
-  overflow-y: auto;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 9;
-  width: 500px;
-  max-height: 320px;
-  height: min-content;
-  padding: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.2);
-  background-color: #202123;
-  opacity: 0.96;
-  color: #aaa;
-  left: 50%;
-  margin-left: -250px;
-  border-radius: 2px;
-  border-top: none;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  font-family: 'Lucida Grande', 'Segoe UI', 'Open Sans', sans-serif;
-}
 .modal__backdrop {
-  // background-color: rgba(0, 0, 0, 0.3);
   position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
   z-index: 1;
+  width: 100%;
+  height: 100%;
 }
 .modal__dialog {
-  position: relative;
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 100;
+  width: 500px;
+  max-height: 350px;
+  height: min-content;
+  padding: 10px;
+  border: 2px solid #89898980;
+  background: #f4f3f2b5;
+  backdrop-filter: blur(5px);
+  color: #000;
+  left: 50%;
+  margin-left: -250px;
+  border-radius: 10px;
+  border-top: none;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  box-shadow: 0 0 30px #f4ffb587;
+  font-family: 'Lucida Grande', 'Segoe UI', 'Open Sans', sans-serif;
+  ::-webkit-scrollbar {
+    background-color: #0000002b;
+    width: 5px;
+    height: 8px;
+  }
+
+  ::-webkit-scrollbar-thumb:window-inactive,
+  ::-webkit-scrollbar-thumb {
+    background: #1a6cd29e;
+    box-shadow: none;
+    border-radius: 0px;
+  }
+
+  ::-webkit-scrollbar-corner {
+    background-color: rgba(42, 42, 42, 0.5);
+  }
   outline: none;
-  z-index: 2;
 }
 .modal__input {
-  color: #CCC;
-  background-color: rgba(255, 255, 255, 0.1);
+  color: #000;
+  background-color: #ffffffb3;
   border: 1px solid #000;
   box-shadow: none;
   border-radius: 2px;
@@ -112,5 +125,15 @@
 }
 .modal__input:focus {
   outline-color: #4f99d3;
+}
+
+.slide-enter-active {
+  transition: all .1s ease-in;
+}
+.slide-leave-active {
+  transition: all .1s ease-in;
+}
+.slide-enter, .slide-leave-to {
+  transform: translateY(-100%);
 }
 </style>

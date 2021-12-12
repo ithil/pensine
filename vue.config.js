aa5178ee1894a3f7a9b0edec7f2b20786eaa9ebe
Ithil <5417774+ithil@users.noreply.github.com>
2021-12-12 16:09:58 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,28 @@ module.exports = {
       externals: ['pensieve'],
       // If you are using Yarn Workspaces, you may have multiple node_modules folders
       // List them all here so that VCP Electron Builder can find them
-      nodeModulesPath: ['../../node_modules', './node_modules']
+      nodeModulesPath: ['../../node_modules', './node_modules'],
+      builderOptions: {
+        appId: "com.ithil.pensine",
+        productName: "Pensine",
+        asar: false,
+        mac: {
+          category: "public.app-category.productivity",
+          target: "dir",
+          extendInfo: {
+            "NSServices": [
+              {
+                "NSSendTypes": ["NSStringPboardType"],
+                "NSMessage": "droppedText",
+                "NSMenuItem": {
+                  "default": "Text Drop"
+                },
+              }
+            ],
+          },
+        },
+      }
     }
   },
-  runtimeCompiler: true
+  runtimeCompiler: true,
 }
